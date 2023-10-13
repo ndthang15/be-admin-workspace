8bd5c0ebcbaffa39a1679c4004c9e1c860c93760
Thang Nguyen <tnguyen2@veritone.com>
2023-10-13 17:34:39 +0700

    lession 2 - lan anh - changes

--- flyway/postgre/migrations/sql/V1_3__Create_lesson2_tables.sql
+++ flyway/postgre/migrations/sql/V1_3__Create_lesson2_tables.sql
@@ -1,76 +1,65 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+-- DROP TYPE IF EXISTS status_type;
+CREATE TYPE status_type AS ENUM ('active', 'suspened', 'deleted');
 
-CREATE TYPE organization_status_type AS ENUM ('active', 'suspend', 'deleted');
-CREATE TYPE user_status_type AS ENUM ('active', 'deactivated', 'deleted');
+-- DROP TYPE IF EXISTS gender_type;
 CREATE TYPE gender_type AS ENUM ('male', 'female');
 
-CREATE TABLE IF NOT EXISTS sso_organization (
-    organization_id SERIAL PRIMARY KEY NOT NULL,
-    organization_name VARCHAR(255) NOT NULL,
-    status organization_status_type DEFAULT 'active'::organization_status_type NOT NULL,
-    address VARCHAR(255),
-    contact_number VARCHAR(255),
-    email VARCHAR(255),
-    country VARCHAR(255),
-    state VARCHAR(255),
-    city VARCHAR(255),
-    postal_code VARCHAR(255),
-    website_url VARCHAR(255),
-    kvp JSONB,
-    date_created TIMESTAMPTZ DEFAULT now(),
-    date_modified TIMESTAMPTZ DEFAULT now()
+create table if not EXISTS sso_organization  (
+    organization_id BIGINT not null generated always as identity primary KEY,
+    organization_name text not null,
+    kvp jsonb,
+    date_created TIMESTAMP,
+    date_modified TIMESTAMP,
+    status status_type not null default 'active',
+    address text,
+    contact_number text,
+    email text,
+    country text,
+    state text,
+    city text,
+    postal_code text,
+    organization_sub_name text,
+    website_url text
 );
 
-CREATE TABLE IF NOT EXISTS sso_role (
-    role_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-    role_name VARCHAR(255) NOT NULL,
-    role_description VARCHAR(255),
-    organization_id INT,
-    FOREIGN KEY (organization_id) REFERENCES sso_organization(organization_id)
+create table if not EXISTS sso_user  (
+user_id BIGINT not null generated always as IDENTITY primary key,
+user_name text not null,
+password text not null,
+kvp json,
+date_created TIMESTAMP,
+date_modified TIMESTAMP,
+modified_by BIGINT,
+last_logged_in TIMESTAMP,
+user_setting json default '{}'::json,
+status status_type default 'active'
 );
 
-CREATE TABLE IF NOT EXISTS sso_user (
-    user_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    kvp JSONB,
-    date_created TIMESTAMPTZ DEFAULT now(),
-    date_modified TIMESTAMPTZ DEFAULT now(),
-    modified_by UUID,
-    last_logged_in TIMESTAMPTZ,
-    user_settings JSONB DEFAULT '{}' :: JSONB,
-    status user_status_type DEFAULT 'active' :: user_status_type
+create table if not EXISTS sso_role  (
+ role_id BIGINT not null generated always as IDENTITY primary key, -- UUID
+ role_name text not null,
+ role_description text,
+ organization_id BIGINT references sso_organization(organization_id),
+ 
 );
 
-CREATE TABLE IF NOT EXISTS sso_user_profile (
-    profile_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    user_id UUID NOT NULL,
-    birth_date DATE,
-    sex gender_type,
-    title VARCHAR(255),
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    avatar VARCHAR(255),
-    age INT,
-    address VARCHAR(255),
-    date_created TIMESTAMPTZ DEFAULT now(),
-    date_modified TIMESTAMPTZ DEFAULT now(),
-    FOREIGN KEY (user_id) REFERENCES sso_user(user_id)
+create table if not EXISTS sso_user_profile  (
+profile_id BIGINT not null generated always as IDENTITY primary key,
+user_id bigint references sso_user(user_id),
+birth_date date,
+sex gender_type,
+title text,
+firstname text not null,
+lastname text not null,
+avatar text,
+age_number int,
+address text,
+date_created TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS sso_organization_user (
-    organization_id BIGINT,
-    user_id UUID,
-    PRIMARY KEY (organization_id, user_id),
-    FOREIGN KEY (organization_id) REFERENCES sso_organization(organization_id),
-    FOREIGN KEY (user_id) REFERENCES sso_user(user_id)
-);
 
-CREATE TABLE IF NOT EXISTS sso_user_role (
-    user_id UUID,
-    role_id UUID,
-    PRIMARY KEY (user_id, role_id),
-    FOREIGN KEY (user_id) REFERENCES sso_user(user_id),
-    FOREIGN KEY (role_id) REFERENCES sso_role(role_id)
+CREATE TABLE IF NOT EXISTS sso_profile_to_role (
+profile_role_id BIGINT not null generated always as IDENTITY primary key,
+profile_id BIGINT references sso_user_profile(profile_id),
+role_id BIGINT references sso_role(role_id)
 );
