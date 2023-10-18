95c9dd0a08a00b60c51a4b22311cf3ced0baa3bd
Thang Nguyen <tnguyen2@veritone.com>
2023-10-18 13:49:38 +0700

    Team 2 lesson 4 commit

--- dal/users.js
+++ dal/users.js
@@ -1,18 +1,18 @@
-const _ = require('lodash');
-const camelcaseKeys = require('camelcase-keys');
+const _ = require("lodash");
+const camelcaseKeys = require("camelcase-keys");
 
 module.exports = (app) => {
   const db = app.pg;
 
   async function getUser(userId, dbClient = db) {
     if (!userId) {
-      throw new Error('userId is required.');
+      throw new Error("userId is required.");
     }
 
     const resultQuery = await getUsers({ userId }, dbClient);
-    
+
     if (_.isEmpty(resultQuery.rows)) {
-      throw new Error('User Not Found');
+      throw new Error("User Not Found");
     }
     return camelcaseKeys(resultQuery.rows[0], { deep: true }); // { userId: 1, ... }
   }
@@ -40,15 +40,15 @@ module.exports = (app) => {
     }
 
     if (sqlWhere.length) {
-			sql += ' WHERE ' + sqlWhere.join(' AND ');
-		}
+      sql += " WHERE " + sqlWhere.join(" AND ");
+    }
 
     if (Number.isInteger(options.limit)) {
-			sql += ` LIMIT ${options.limit || 30}`;
-		}
-		if (Number.isInteger(options.offset)) {
-			sql += ` OFFSET ${options.offset || 0}`;
-		}
+      sql += ` LIMIT ${options.limit || 30}`;
+    }
+    if (Number.isInteger(options.offset)) {
+      sql += ` OFFSET ${options.offset || 0}`;
+    }
 
     const resultQuery = await dbClient.query(sql, sqlParams, (result) => {
       return camelcaseKeys(result.rows, { deep: true });
@@ -58,13 +58,13 @@ module.exports = (app) => {
       count: resultQuery.length,
       results: resultQuery,
       limit: options.limit || 30,
-      offset: options.offset || 0
-    }
+      offset: options.offset || 0,
+    };
   }
 
   async function getUserOrganizations(userId, dbClient = db) {
     if (!userId) {
-      throw new Error('userId is required.');
+      throw new Error("userId is required.");
     }
 
     const sql = `
@@ -91,5 +91,152 @@ module.exports = (app) => {
     return camelcaseKeys(resultQuery.rows, { deep: true });
   }
 
-  return { getUser, getUsers, getUserOrganizations };
+  async function createUser(body, dbClient = db) {
+    let errorMessage = "";
+    let count = 0;
+    let sql = ``;
+    const {
+      username,
+      password,
+      email,
+      kvp,
+      modified_by,
+      last_logged_in,
+      user_settings,
+      status,
+    } = body;
+    if (!username) {
+      errorMessage += "username, ";
+      count += 1;
+    }
+    if (!email) {
+      errorMessage += "email, ";
+      count += 1;
+    }
+    if (!password) {
+      errorMessage += "password ";
+      count += 1;
+    }
+    error =
+      count === 0
+        ? ``
+        : count === 1
+        ? error + " is required."
+        : " are required.";
+
+    sql += `
+      INSERT INTO sso_user (
+        user_id,
+        username,
+        password,
+        email,
+        status,
+        kvp,
+        modified_by,
+        last_logged_in,
+        user_settings
+      )
+      VALUES(
+        uuid_generate_v4(),
+        '${username}',
+        '${password}',
+        '${email}',
+        '${status ? status : "active"}',
+    `;
+    if (kvp) {
+      sql += `'${JSON.stringify(kvp)}',`;
+    } else {
+      sql += `'{}',`;
+    }
+    if (modified_by) {
+      sql += `'${modified_by}',`;
+    } else {
+      sql += `NULL,`;
+    }
+    if (last_logged_in) {
+      sql += `'${modified_by}',`;
+    } else {
+      sql += `NULL,`;
+    }
+    if (user_settings) {
+      sql += `'${JSON.stringify(modified_by)}'`;
+    } else {
+      sql += `'{}'`;
+    }
+    sql += `);`;
+
+    const resultQuery = await dbClient.query(sql);
+    return camelcaseKeys(resultQuery.rows, { deep: true });
+  }
+
+  async function getUserById(userId, dbClient = db) {
+    if (!userId) {
+      throw new Error("userId is required");
+    }
+
+    let sql = `SELECT * FROM sso_user WHERE user_id = '${userId}'`;
+    const resultQuery = await dbClient.query(sql);
+    return camelcaseKeys(resultQuery.rows, { deep: true });
+  }
+
+  async function deleteUser(userId, dbClient = db) {
+    if (!userId) {
+      throw new Error("userId is required");
+    }
+    const exitingUser = await getUserById(userId, dbClient);
+    if (exitingUser.length === 0) {
+      throw new Error(`user with id ${userId} does not exist`);
+    }
+    let sql = `DELETE FROM sso_user WHERE user_id = '${userId}';`;
+    const resultQuery = await dbClient.query(sql);
+    return camelcaseKeys(resultQuery.rows, { deep: true });
+  }
+
+  async function updateUser(userId, body, dbClient = db) {
+    const { username, password, email, kvp, user_settings, status } = body;
+    if (!userId) {
+      throw new Error("userId is required");
+    }
+    const exitingUser = await getUserById(userId, dbClient);
+    if (exitingUser.length === 0) {
+      throw new Error(`user with id ${userId} does not exist`);
+    }
+    let sql = ``;
+    sql += `
+      UPDATE sso_user SET
+    `;
+
+    if (username) {
+      sql += `username = '${username}',`;
+    }
+    if (username) {
+      sql += `password = '${password}',`;
+    }
+    if (email) {
+      sql += `email = '${email}',`;
+    }
+    if (kvp) {
+      sql += `kvp = '${JSON.stringify(kvp)}',`;
+    }
+    if (user_settings) {
+      sql += `user_settings = '${JSON.stringify(user_settings)}',`;
+    }
+    if (email) {
+      sql += `status = '${status}'`;
+    }
+    sql += `;`;
+
+    const resultQuery = await dbClient.query(sql);
+    return camelcaseKeys(resultQuery.rows, { deep: true });
+  }
+
+  return {
+    getUser,
+    getUsers,
+    getUserOrganizations,
+    createUser,
+    getUserById,
+    deleteUser,
+    updateUser,
+  };
 };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 
 module.exports = function (app, middelwares) {
   if (!app) {
-    throw new Error('missing app');
+    throw new Error("missing app");
   }
 
   const routerUsers = express.Router();
@@ -10,17 +10,66 @@ module.exports = function (app, middelwares) {
   //TODO: Add Router-level middlewares
 
   //TODO: Add the Route-level middleware to response 401 if any falling through
-  app.use('/users', routerUsers);
+  app.use("/users", routerUsers);
 
-  //TODO: Add the routes for organization model
-  routerUsers.get('/', getUsers);
-
+  //Get all users
+  routerUsers.get("/", getUsers);
   async function getUsers(req, res, next) {
     try {
       // validation params if needed
       const options = req.query;
       const results = await app.bll.users.getUsers(options);
 
+      return res.status(200).send(results);
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  //Add new user
+  routerUsers.post("/", createUser);
+  async function createUser(req, res, next) {
+    try {
+      const body = req.body;
+      const results = await app.bll.users.createUser(body);
+
+      return res.status(200).send(results);
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  //Get user
+  routerUsers.get("/:id", getUserById);
+  async function getUserById(req, res, next) {
+    try {
+      const id = req.params.id;
+      const results = await app.bll.users.getUserById(id);
+      return res.status(200).send(results);
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  //Update user
+  routerUsers.patch("/:id", updateUser);
+  async function updateUser(req, res, next) {
+    try {
+      const id = req.params.id;
+      const body = req.body;
+      const results = await app.bll.users.updateUser(id, body);
+      return res.status(200).send(results);
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  //Delete user
+  routerUsers.delete("/:id", deleteUser);
+  async function deleteUser(req, res, next) {
+    try {
+      const id = req.params.id;
+      const results = await app.bll.users.deleteUser(id);
       return res.status(200).send(results);
     } catch (err) {
       return next(err);
