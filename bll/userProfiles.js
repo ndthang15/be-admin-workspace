4d5987f5a04128917e1f79e2e7a18e07a55cf4e8
Thang Nguyen <thang.nguyen@setacinq.vn>
2023-11-01 17:45:36 +0700

    Update bll/userProfiles.js

--- bll/userProfiles.js
+++ bll/userProfiles.js
@@ -1,8 +1,8 @@
 const _ = require('lodash');
 
 module.exports = (app) => {
-  async function getUserProfile(roleId) {
-    const userProfile = await app.dal.userProfiles.getUserProfile(roleId);
+  async function getUserProfile(userId) {
+    const userProfile = await app.dal.userProfiles.getUserProfile(userId);
 
     return userProfile;
   }
